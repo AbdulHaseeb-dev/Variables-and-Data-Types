ae49a39b929d7db745605d3b76793766023bcd7d
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-06-02 01:00:39 +0500

    Practice Question 2 Completed

--- PracticeQuestion.java
+++ PracticeQuestion.java
@@ -2,18 +2,37 @@ import java.util.Scanner;
 
 public class PracticeQuestion {
     public static void main(String[] args) {
+
+        // Question 1 : In a program,input 3 numbers : A,B and C.You have to output the average of these 3 numbers.(Hint : Average of N numbers is sum of those numbers divided by N)
+
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a");
+        // int a = sc.nextInt();
+        // System.out.println("Enter b");
+        // int b = sc.nextInt();
+        // System.out.println("Enter c");
+        // int c = sc.nextInt();
+
+        // int d = (a + b + c)/3;
+
+        // System.out.println("Average is : " + d);
+
+        // sc.close();
+
+        // Question 2 : In a program, input the side of a square.You have to output the area of the square.(Hint : area of a square is (side x side))
+         
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a");
-        int a = sc.nextInt();
-        System.out.println("Enter b");
-        int b = sc.nextInt();
-        System.out.println("Enter c");
-        int c = sc.nextInt();
+        System.out.println("Enter Length");
+        int side = sc.nextInt();
 
-        int d = (a + b + c)/3;
+        int Area_of_Square = side * side;
 
-        System.out.println(d);
+        System.out.println("Area of Square is : " + Area_of_Square);
 
         sc.close();
+
+
+
     }
 }
